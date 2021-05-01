2a3e5b0b5ab391cb914349c1aba5779b83fb0a4b
Aveshen17 <81326165+Aveshen17@users.noreply.github.com>
2021-05-01 14:30:53 +0200

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -54,6 +54,7 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 # Module 5 code here:
+
 energy_data=EnergyData(data)
 recs=energy_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
